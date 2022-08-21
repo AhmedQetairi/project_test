3aca63bfd4e8d61a6ba85c013a9478839cc6a537
AhmedQetairi <ahmad1996_2014@hotmail.com>
2022-08-21 21:34:44 +0000

    update as rawan need

--- hotel/models/hotel_room.py
+++ hotel/models/hotel_room.py
@@ -20,6 +20,8 @@ class HotelRoom(models.Model):
     _name = "hotel.room"
     _description = "Hotel Room"
 
+    image = fields.Binary()
+    
     product_id = fields.Many2one(
         "product.product",
         "Product_id",
@@ -35,9 +37,12 @@ class HotelRoom(models.Model):
     )
     max_adult = fields.Integer()
     max_child = fields.Integer()
+    
     room_categ_id = fields.Many2one(
         "hotel.room.type", "Billboard Category", required=True, ondelete="restrict"
     )
+    
+      
     room_amenities_ids = fields.Many2many(
         "hotel.room.amenities", string="Room Amenities", help="List of room amenities."
     )
@@ -51,6 +56,8 @@ class HotelRoom(models.Model):
         "folio.room.line", "room_id", string="Billboard Reservation Line"
     )
     product_manager = fields.Many2one("res.users", "Product Manager")
+    
+    
 
     @api.model
     def create(self, vals):
